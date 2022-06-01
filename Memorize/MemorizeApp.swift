d036111b90a9dc42dae6ef16c995f57f2c48f712
Felipe <felipeavmuller@gmail.com>
2022-05-31 22:57:23 -0400

    starting project

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by user222503 on 5/31/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
